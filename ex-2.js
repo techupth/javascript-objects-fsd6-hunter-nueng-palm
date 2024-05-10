2c6f9e92524520b1c84aa51122fc59436244d963
nueng <wongsaokrn.kump@gmail.com>
2024-05-10 16:37:36 +0700

    fix: fix key value

--- ex-2.js
+++ ex-2.js
@@ -11,6 +11,6 @@ product["ratings"] = 4.5
 product["photo"] = "https://i01.appmifile.com/webfile/globalimg/products/pc/mi-air-purifier-3H/replace_03.jpg"
 product["code"] = "IT0101010"
 delete product["ratings"]
-product["socialMediaShare"] = 45.5
+product[socialMediaShare] = 45.5
 console.log(product)
 
