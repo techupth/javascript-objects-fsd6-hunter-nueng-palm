ae5216d88691be9d9527d89ac6eedd817a6b9a0f
nueng <wongsaokrn.kump@gmail.com>
2024-05-10 13:09:04 +0700

    feat: add answer code

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,12 @@
 // Exercise #1: Food Order
 
-// Start coding here
+let foodOrder ={
+    orderrNumber: "A0234",
+    address: "Bangkok",
+    paymentType: "Cash",
+    totalPrice: 4500,
+    restaurantName:"MK",
+}
+foodOrder["paymentType"] = "Credit Card"
+foodOrder["totalPrice"] = 5000
+console.log(foodOrder)
